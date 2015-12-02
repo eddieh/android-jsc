bc715c1f0b533912e6b626d3a01b2b70c829c247
Eddie Hillenbrand <eddie.hillenbrand@adcolony.com>
2015-12-01 16:18:15 -0800

    Test runner and basic tests for Unicode support.

--- src/com/adcolony/jsctest/JSCTest.java
+++ src/com/adcolony/jsctest/JSCTest.java
@@ -17,11 +17,31 @@ package com.adcolony.jsctest;
 
 import android.app.Activity;
 import android.widget.TextView;
-import android.os.Bundle;
-
+import android.os.*;
+import android.util.*;
+import java.io.*;
+import java.util.*;
 
 public class JSCTest extends Activity
 {
+    byte[] loadAsset(String asset)
+    {
+        if (asset == null) return null;
+
+        try {
+            InputStream instream = getAssets().open(asset);
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream(instream.available());
+            for (int ch = instream.read(); ch != -1; ch = instream.read()) {
+                buffer.write(ch);
+            }
+
+            return buffer.toByteArray();
+        } catch (IOException err) {
+            Log.e("JSCTest", "Asset file not found: " + asset);
+            return null;
+        }
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -34,8 +54,49 @@ public class JSCTest extends Activity
          */
         TextView tv = new TextView(this);
         //tv.setText(stringFromJSC());
-        tv.setText(runTest("var test = function () { return 'Hello from runTest!\\n' }".getBytes()));
-        setContentView(tv);
+        //tv.setText(runTest("var test = function () { return 'Hello from runTest!\\n' }".getBytes()));
+
+        // TODO: cumulative pass/fail count
+        // NOTE: results cannot begin or end with white space
+        try {
+          String[] assets = this.getAssets().list("");
+
+          // filter the list
+          List<String> tests = new ArrayList<String>();
+          for (String file : assets) {
+            if (file.endsWith(".js")) tests.add(file);
+          }
+
+          String str = "";
+          for (String testFile : tests) {
+            // read the test.js
+            byte[] testScript = this.loadAsset(testFile);
+
+            // drop .js and add .expected from filename
+            String expectedFile = testFile.replaceAll(".js", "");
+            expectedFile = expectedFile += ".expected";
+
+            // read the test.expected
+            String expectedResults = (new String(this.loadAsset(expectedFile), "UTF-8")).trim();
+
+            // run the test
+            String testResults = this.runTest(testScript).trim();
+
+            Log.e("JSCTest", "testResult: " + testResults);
+            Log.e("JSCTest", "expectedResult: " + expectedResults);
+
+            // compare the actual result with the expected result
+            if (testResults.equals(expectedResults)) {
+              str += testFile += " PASS\n";
+            } else {
+              str += testFile += " FAIL\n";
+            }
+          }
+
+          tv.setText(str);
+        } catch (IOException e) {}
+
+        this.setContentView(tv);
     }
 
     /* A native method that is implemented by the
